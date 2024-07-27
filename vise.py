fa2a302e1646685e467d648b9ca30fa657b9639d
David <rynstheoverlord@gmail.com>
2024-07-27 16:21:38 +0100

    Added window!

--- vise.py
+++ vise.py
@@ -4,16 +4,17 @@ import time
 
 
 def main(stdscr):
-
+    stdscr.clear()
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
 
-    window = curses.newwin()
+
+    window_height, window_width = stdscr.getmaxyx()
+    window = curses.newwin(window_height, window_width, 0, 0)
     for i in range(10):
-        stdscr.clear()
-        stdscr.addstr(f"Enter file path [{i + 1}]: ", curses.color_pair(1))
-        stdscr.refresh()
+        window.clear()
+        window.addstr(f"Enter file path [{i + 1}]: ", curses.color_pair(1))
+        window.refresh()
         time.sleep(0.5)
-    stdscr.getch() # Initialize the window and set the border
     
 
 wrapper(main)
