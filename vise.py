808d3d5ba2544b6ae87b9519027e4c4cad3c9126
David <107300355+lordryns@users.noreply.github.com>
2024-07-28 21:11:38 +0000

    Fixed path issue and initialized text editor screen!

--- vise.py
+++ vise.py
@@ -8,13 +8,14 @@ console = Console()
 console.set_window_title("Vise 1.0")
 console.clear()
 
-current_file = None 
+current_file = ""
 
 def open_file_to_edit() -> bool:
 	global current_file
 	run = True 
 	while run: 
 		path = console.input("Enter the path of the file you want to edit: ")
+		path = path.replace(" ", "")
 
 		if os.path.exists(path):
 			current_file = path
@@ -34,11 +35,16 @@ def open_file_to_edit() -> bool:
 
 can_launch_editor: bool = open_file_to_edit()
 
+
+# the editor screen 
+def text_editor(file_path: str) -> None:
+	console.print(f"file open: [green]{file_path}")
+
 if can_launch_editor: 
 	console.print("Hold on, setting up editor...")
 	with console.status("setting up..."):
 		time.sleep(1)
 		console.clear()
-		console.print("Done!", style="green")
+		text_editor(file_path=current_file)
 
 
