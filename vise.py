ce61f527a44702ef8dc15ced9974c8aac3678729
David <107300355+lordryns@users.noreply.github.com>
2024-07-28 14:57:59 +0000

    Experimenting with the rich package.

--- vise.py
+++ vise.py
@@ -1,20 +1,19 @@
-import curses
-from curses import wrapper
+import rich 
+from rich.console import Console
 import time
 
 
-def main(stdscr):
-    stdscr.clear()
-    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+console = Console()
+
+console.print("[cyan]Hello [/]world, this was built with [yellow]Python.")
+
+name = console.input("Enter your name: ")
+with console.status("Loading", spinner='aesthetic'):
+	time.sleep(3)
+	console.print("Almost there...")
+	time.sleep(3)
+	console.print("Done!", style="green")
+	time.sleep(1)
 
 
-    window_height, window_width = stdscr.getmaxyx()
-    window = curses.newwin(window_height, window_width, 0, 0)
-    for i in range(10):
-        window.clear()
-        window.addstr(f"Enter file path [{i + 1}]: ", curses.color_pair(1))
-        window.refresh()
-        time.sleep(0.5)
-    
-
-wrapper(main)
+console.clear()
